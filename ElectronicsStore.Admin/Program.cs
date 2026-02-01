3524fec0b74083c22950097257fa0277f6195efa
nqvlwu <nguyenquocviet12062005@gmail.com>
2026-02-01 16:06:30 +0700

    feat: add login and product modules, clean build files, update .gitignore

--- ElectronicsStore.Admin/Program.cs
+++ ElectronicsStore.Admin/Program.cs
@@ -8,6 +8,13 @@ namespace ElectronicsStore.Admin
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
@@ -23,12 +30,13 @@ namespace ElectronicsStore.Admin
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseSession();
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Login}/{action=Index}/{id?}");
 
             app.Run();
         }
